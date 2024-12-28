bdc2d449c446f0e60d29bd62d01716b45c209670
Kirti Verma <kirti.verma@swiggy.in>
2024-12-28 11:58:19 +0530

    Initial Commit

---
+++ BrotiiSample/BrotiiSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrotiiSampleApp.swift
+//  BrotiiSample
+//
+//  Created by Kirti Kumar Verma on 28/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BrotiiSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
